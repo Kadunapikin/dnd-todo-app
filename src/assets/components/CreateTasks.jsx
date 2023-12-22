6399453dcea9a5d9f69434bf8cdfec23440cf393
Kadunapikin <ibakoshi27@gmail.com>
2023-12-22 14:05:02 +0100

    style form input and button in CreateTasks.jsx

--- src/assets/components/CreateTasks.jsx
+++ src/assets/components/CreateTasks.jsx
@@ -1,9 +1,10 @@
 
 const CreateTasks = ({ tasks, setTasks }) => {
     return (
-        <div>
-            Create Task
-        </div>  
+        <form>
+            <input type="text" className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1" />
+            <button className="bg-cyan-500 h-12 rounded-md px-4">Create</button>
+        </form>
     )
 }
 
