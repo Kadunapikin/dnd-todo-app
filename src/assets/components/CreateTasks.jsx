7a0180130cd2354e7061a105b63ba3d8360a8029
Kadunapikin <ibakoshi27@gmail.com>
2023-12-22 21:23:11 +0100

    update form input in CreateTasks.jsx

--- src/assets/components/CreateTasks.jsx
+++ src/assets/components/CreateTasks.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const CreateTasks = ({ tasks, setTasks }) => {
+const [task, setTask] = useState({
+    id: "",
+    name: "",
+    status: "todo" //could also be in progress or done
+})
+
+console.log(task);
+
     return (
         <form>
-            <input type="text" className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1" />
+            <input type="text" 
+            className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1" 
+            value={task.name}
+            onChange={(e) => setTask({ ...task, id: uuidv4(), name: e.target.value })}
+            />
             <button className="bg-cyan-500 h-12 rounded-md px-4">Create</button>
         </form>
     )
