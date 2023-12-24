cd8e5eb179c8b656934c0b83c3d89a8fd57779f3
Kadunapikin <ibakoshi27@gmail.com>
2023-12-24 09:20:55 +0100

    add handleSubmit function to CreateTasks.jsx

--- src/assets/components/CreateTasks.jsx
+++ src/assets/components/CreateTasks.jsx
@@ -10,8 +10,17 @@ const [task, setTask] = useState({
 
 console.log(task);
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setTasks((prev) => {
+        const list = [...prev, task];
+        localStorage.setItem("tasks", JSON.stringify(list));
+        return list
+    });
+};
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" 
             className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1" 
             value={task.name}
