af83dd6024434feee2fedc528c3b5fff42d42d0f
Kadunapikin <ibakoshi27@gmail.com>
2023-12-25 14:39:52 +0100

    style Header.jsx component

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,7 +1,10 @@
-const Header = ({ text, bg }) => {
+const Header = ({ text, bg, count }) => {
     return(
-        <div>
-            <h1>{text}</h1>
+        <div className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white `}>
+            {text}
+            <div className="ml-2 bg-white text-black w-5 h-5 rounded-full flex items-center justify-center">
+                {count}
+            </div>
         </div>
     )
 }
--- src/assets/components/Section.jsx
+++ src/assets/components/Section.jsx
@@ -4,7 +4,7 @@ const Section = ({ status, tasks, setTasks, todos, inprogress, completed }) => {
     const text = "Todo";
     const bg = "bg-slate-500";
     return(
-        <div>
+        <div className="w-64">
             <Header text={text} bg={bg} count={todos.length} /> List
         </div>
     )
