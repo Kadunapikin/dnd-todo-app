ea83a4762dc7858d88a6cb2ce71f038f5ecf1216
Kadunapikin <ibakoshi27@gmail.com>
2023-12-25 14:07:36 +0100

    declare text and bg variables on Section.jsx component

--- src/assets/components/Section.jsx
+++ src/assets/components/Section.jsx
@@ -1,4 +1,6 @@
 const Section = ({ status, tasks, setTasks, todos, inprogress, completed }) => {
+    const text = "Todo";
+    const bg = "bg-slate-500";
     return(
         <div>
             <h1>{status}</h1> List
