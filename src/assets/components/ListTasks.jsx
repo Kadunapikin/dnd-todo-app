be76f7827620b86daf78f00b1cf4db2e12f6187c
Kadunapikin <ibakoshi27@gmail.com>
2023-12-25 12:40:43 +0100

    create a useEffect and save filtered results in differend variables

--- src/assets/components/ListTasks.jsx
+++ src/assets/components/ListTasks.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
 
 const ListTasks = ({ tasks, setTasks }) => {
-    const [todos, setTodos] = useState([])
-    const [inprogress, setInprogress] = useState([])
-    const [completed, setCompleted] = useState([])
+    const [todos, setTodos] = useState([]);
+    const [inprogress, setInprogress] = useState([]);
+    const [completed, setCompleted] = useState([]);
+
+    useEffect(() => {
+        const fTodos = tasks.filter(task => task.status === "todo");
+        const fInprogress = tasks.filter(task => task.status === "inprogress");
+        const fCompleted = tasks.filter(task => task.status === "completed");
+
+    }, [tasks])
 
 
     return (
