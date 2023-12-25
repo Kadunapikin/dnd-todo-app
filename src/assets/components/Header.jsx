cd5f13622aef805fe301d9f378fec22e2d53f15d
Kadunapikin <ibakoshi27@gmail.com>
2023-12-25 14:17:47 +0100

    create Header,jsx component and import it to Section.jsx

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = ({ text, bg }) => {
+    return(
+        <div>
+            <h1>{text}</h1>
+        </div>
+    )
+}
+export default Header;
--- src/assets/components/Section.jsx
+++ src/assets/components/Section.jsx
@@ -1,9 +1,11 @@
+import Header from "./Header";
+
 const Section = ({ status, tasks, setTasks, todos, inprogress, completed }) => {
     const text = "Todo";
     const bg = "bg-slate-500";
     return(
         <div>
-            <h1>{status}</h1> List
+            <Header text={text} bg={bg} count={todos.length} /> List
         </div>
     )
 }
