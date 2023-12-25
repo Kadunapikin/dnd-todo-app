9771cc93a545caf829c15cf591197ac29fa68a6d
Kadunapikin <ibakoshi27@gmail.com>
2023-12-25 12:23:05 +0100

    create useState for todos, inprogress and completed tasks

--- src/assets/components/ListTasks.jsx
+++ src/assets/components/ListTasks.jsx
@@ -1,5 +1,11 @@
+import { useEffect, useState } from "react";
 
 const ListTasks = ({ tasks, setTasks }) => {
+    const [todos, setTodos] = useState([])
+    const [inprogress, setInprogress] = useState([])
+    const [completed, setCompleted] = useState([])
+
+
     return (
         <div>
             List Tasks
